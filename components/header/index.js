3f5fc76b9b7ec0d125b68744c162467c9fa314c3
Kaylana1968 <kristian.chou.fr@gmail.com>
2026-05-20 11:09:06 +0200

    add: favicon + adjusted images/logo.png so it is better to work with + some small fixes

--- components/header/index.js
+++ components/header/index.js
@@ -5,8 +5,8 @@ class CustomHeader extends HTMLElement {
 		this.innerHTML = `
       <header class="sticky flex items-center top-0 left-0 h-12 sm:h-20 bg-white border-b border-b-gray-300 shadow-md z-10">
         <a href="index.html" class="flex items-center h-full w-min sm:pr-4 select-none">
-          <div class="h-full aspect-square">
-            <img src="images/logo.png" height="500" width="500" />
+          <div class="h-full aspect-square p-4">
+            <img src="images/logo.png" height="300" width="300" />
           </div>
           <span class="hidden sm:inline whitespace-nowrap text-orange-500 text-3xl"> Kook Ink </span>
         </a>
