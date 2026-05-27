0b4db5189a2a5fa7d6a025c351bb934dce5d1283
Kaylana1968 <kristian.chou.fr@gmail.com>
2026-05-27 12:41:30 +0200

    add: pricing.html page

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ import CustomMain from "./components/main.js";
 import NoResult from "./components/header/no-result.js";
 import ContactCard from "./components/contact-card.js";
 import FaqCard from "./components/faq-card.js";
+import PricingCard from "./components/pricing-card.js";
 
 customElements.define("custom-header", CustomHeader);
 customElements.define("custom-main", CustomMain);
 customElements.define("no-result", NoResult);
 customElements.define("contact-card", ContactCard);
 customElements.define("faq-card", FaqCard);
+customElements.define("pricing-card", PricingCard);
 
 window.addEventListener("load", () => {
 	const hash = window.location.hash;
